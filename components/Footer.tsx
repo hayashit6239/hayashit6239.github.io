cb7bc2a45f9a2123db628e5e39590fe46d45c301
hayashit6239 <hyshtmnr6239dcm@gmail.com>
2025-11-07 10:52:24 +0900

    [add] フッターの表示を整理

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Container, Text, Group, Stack, Anchor } from '@mantine/core';
-import { IconBrandGithub, IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandGithub, IconBrandX } from '@tabler/icons-react';
 
 export function Footer() {
   return (
@@ -10,31 +10,20 @@ export function Footer() {
           <Group justify="space-between" align="flex-start">
             <div>
               <Text size="lg" fw={600} className="mb-2">
-                Google Cloud Technical Summit 2025
+                Google Cloud Technical Summit 2026
               </Text>
               <Text size="sm" c="dimmed">
                 powered by Jagu&apos;é&apos;r
               </Text>
-              <Text size="xs" c="dimmed" mt="xs">
-                超技術特化のGoogle Cloudカンファレンス
-              </Text>
             </div>
             <Group gap="md">
               <Anchor
-                href="https://github.com"
+                href="https://x.com/gctsjp"
                 target="_blank"
                 className="text-gray-400 hover:text-white transition-colors"
-                aria-label="GitHub"
+                aria-label="X"
               >
-                <IconBrandGithub size={24} />
-              </Anchor>
-              <Anchor
-                href="https://twitter.com"
-                target="_blank"
-                className="text-gray-400 hover:text-white transition-colors"
-                aria-label="Twitter"
-              >
-                <IconBrandTwitter size={24} />
+                <IconBrandX size={24} />
               </Anchor>
             </Group>
           </Group>
@@ -42,7 +31,7 @@ export function Footer() {
             <Text size="xs" c="dimmed">
               © 2025 Jagu&apos;é&apos;r. All rights reserved
             </Text>
-            <Group gap="md">
+            {/* <Group gap="md">
               <Anchor
                 href="#"
                 size="xs"
@@ -57,7 +46,7 @@ export function Footer() {
               >
                 お問い合わせ
               </Anchor>
-            </Group>
+            </Group> */}
           </Group>
         </Stack>
       </Container>
